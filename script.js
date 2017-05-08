60021b6fd60eb554ddd825ae0fcb8be74732fb26
Elisabeth Diana Kartika S <elisabeth.dianaks@gmail.com>
2017-05-08 18:28:27 +0700

    Angular routing

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // script.js
 
     // create the module and name it scotchApp
-    var scotchApp = angular.module('scotchApp', []);
+        // also include ngRoute for all our routing needs
+    var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+    // configure our routes
+    scotchApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'static/list-users.html',
+                controller  : 'mainController'
+            })
+    });
 
     // create the controller and inject Angular's $scope
     scotchApp.controller('mainController', function($scope) {
-
         // create a message to display in our view
         $scope.message = 'Everyone come and see how good I look!';
     });
