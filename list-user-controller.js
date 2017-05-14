0d5d37b7cb880a6d0f778769f45e9e47c5c182eb
Elisabeth Diana Kartika S <elisabeth.dianaks@gmail.com>
2017-05-14 13:26:33 +0700

    Create user list controllet, create http get users and show in table

---
+++ list-user-controller.js
@@ -0,0 +1,11 @@
+ angular.module('osloApp').controller('userListController',['$scope','$http',
+ function  ($scope,$http) {
+     var url = "localhost:8080/api/users?warehouse=Cakung";
+     $http.get(url).then( function(response) {
+         $scope.response = response;
+     });
+
+     function relocate_register(){
+           location.href = "#/list-users/register-user";
+      }
+ }]);
