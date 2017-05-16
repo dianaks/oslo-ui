3065ef73d4ea2113f99eab78facf880bf5deb41a
Elisabeth Diana Kartika S <elisabeth.dianaks@gmail.com>
2017-05-16 11:30:47 +0700

    Create form handling for Register User page, Add SweetAlert, Fix list user get problem

--- list-user-controller.js
+++ list-user-controller.js
@@ -1,6 +1,6 @@
  angular.module('osloApp').controller('userListController',['$scope','$http',
  function  ($scope,$http) {
-     var url = "localhost:8080/api/users?warehouse=Cakung";
+     var url = "http://localhost:8080/api/users?warehouse=Cakung";
      $http.get(url).then( function(response) {
          $scope.response = response;
      });
--- register-user-controller.js
+++ register-user-controller.js
@@ -1,24 +1,26 @@
 angular.module('osloApp').controller('registerUserController',['$scope','$http',
 function  ($scope,$http) {
+
+    $scope.newUser = {};
+    $('.selectpicker').selectpicker('render');
+
     $scope.registerUser = function(){
-      var newUser = {
-        username:"demo-counter-five",
-        password:"password",
-        status:"Active",
-        role:"ROLE_COUNTER",
-        warehouse:['Cawang']
-      }
       var request = {
         method: "POST",
         url: "http://localhost:8080/api/users",
-        data: newUser,
+        data: $scope.newUser,
         headers: {
           "Authorization": "Basic " + btoa("super-admin-demo:123")
         }
       }
-      console.log(request);
       $http(request).then(function(response) {
-          console.log(response)
+        if(response.data.success==true){
+            swal("Congrats!", "User "+$scope.newUser.username+" has been registered", "success");
+            $scope.newUser = {};
+        }
+        else{
+            swal("Error!", response.data.errorMessage, "error");
+        }
       });
     }
 }]);
