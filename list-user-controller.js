7086f5ed18ca5a946658bf473dafc484933ee71b
Elisabeth Diana Kartika S <elisabeth.dianaks@gmail.com>
2017-06-02 11:18:17 +0700

    Create daily report modul

--- list-user-controller.js
+++ list-user-controller.js
@@ -1,13 +1,22 @@
  angular.module('osloApp').controller('userListController',['$scope','$http',
  function  ($scope,$http) {
-     var url = "http://localhost:8080/api/users?warehouse=Cakung";
-     $http.get(url).then( function(response) {
-         $scope.response = response;
+     var request = {
+       method: "GET",
+       url: "http://localhost:8080/api/users?warehouse=Cakung",
+       headers: {
+         "Content-Type": "application/json",
+         "Authorization":"Basic " + btoa("super-admin-demo:123")
+       }
+     }
+     $http(request).then(function(response) {
+       $scope.response = response;
      });
 
      function relocate_register(){
            location.href = "#/list-users/register-user";
       }
+
+
 
 
  }]);
