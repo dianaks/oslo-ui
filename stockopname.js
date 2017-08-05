13f6b0bf2e40f6dd93349c55edb6ed6d705e1bf8
Elisabeth Diana Kartika S <elisabeth.dianaks@gmail.com>
2017-08-05 17:56:40 +0700

    Menambahkan header authorisasi stockopname

--- stockopname.js
+++ stockopname.js
@@ -2,12 +2,26 @@
 var stockopnameController = angular.module('osloApp');
 
 stockopnameController.controller('stockopnameController',['$scope','$http', function  ($scope,$http) {
-    var url = "http://localhost:8080/api/stockopnames";
 
-    $http.get(url).then( function(response) {
-        $scope.datas = response;
-        var length = Object.keys(response.data).length;
 
-    });
+	var request = {
+       method: "GET",
+       url: "http://localhost:8080/api/stockopnames",
+       headers: {
+         "Content-Type": "application/json",
+         "Authorization":"Basic " + btoa("admin-demo-one:123")
+       }
+     }
+     $http(request).then(function(response) {
+       $scope.datas = response;
+     });
+
+    // var url = "http://localhost:8080/api/stockopnames";
+
+    // $http.get(url).then( function(response) {
+    //     $scope.datas = response;
+    //     var length = Object.keys(response.data).length;
+
+    // });
 }])
 ;
